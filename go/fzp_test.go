755ab873bf592a5d62065ecbe02a1b48aad02cca
paulvollmer <paul.vollmer@fh-potsdam.de>
2015-07-31 01:08:29 +0200

    wip at fzp CheckTags test

--- go/fzp_test.go
+++ go/fzp_test.go
@@ -7,18 +7,29 @@ import (
 func Test_ReadFzp_Ok(t *testing.T) {
 	f, err := ReadFzp("../sample.fzp")
 	if err != nil {
-		t.Error("Fzp.ReadFzp failed")
+		t.Error("Fzp.ReadFzp broken")
 	}
 
 	errCheck := f.Check()
 	if errCheck != nil {
 		t.Error("Fzp.Check broken:", errCheck)
 	}
+
 }
 
 func Test_ReadFzp_Failed(t *testing.T) {
 	_, err := ReadFzp("../not.found")
 	if err == nil {
-		t.Error("fzp ReadFzp failed")
+		t.Error("Fzp.ReadFzp (that doesn't exists) broken")
+	}
+}
+
+func Test_ReadFzp_CheckTags(t *testing.T) {
+	// fake data
+	fzpData := Fzp{}
+	fzpData.Tags = append(fzpData.Tags, "")
+	// was an error returned?
+	if err := fzpData.CheckTags(); err == nil {
+		t.Error("Fzp.CheckTags broken")
 	}
 }
