16e9382edb7a05f964c167031ae752958c72583f
paulvollmer <paul.vollmer@fh-potsdam.de>
2015-07-30 14:31:55 +0200

    wip at ReadFzp test

--- go/fzp_test.go
+++ go/fzp_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestFzp(t *testing.T) {
+func Test_ReadFzp_Ok(t *testing.T) {
 	f, err := ReadFzp("../sample.fzp")
 	if err != nil {
 		t.Error("fzp ReadFzp failed")
@@ -15,5 +15,12 @@ func TestFzp(t *testing.T) {
 	}
 	if f.Title != "part-name" {
 		t.Error("fzp Title not equal")
+	}
+}
+
+func Test_ReadFzp_Failed(t *testing.T) {
+	_, err := ReadFzp("../not.found")
+	if err == nil {
+		t.Error("fzp ReadFzp failed")
 	}
 }
