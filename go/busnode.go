5c6188b0839018bc0504fc79d7fd8e89fd35d832
paulvollmer <paul.vollmer@fh-potsdam.de>
2015-08-11 11:46:19 +0200

    BusNode added New func and test

--- go/busnode.go
+++ go/busnode.go
@@ -1,5 +1,22 @@
 package fzp
 
-type BusNodeMember struct {
+import (
+	"errors"
+)
+
+type BusNode struct {
 	ConnectorId string `xml:"connectorId,attr"`
+}
+
+func NewBusNode(cId string) BusNode {
+	b := BusNode{}
+	b.ConnectorId = cId
+	return b
+}
+
+func (b *BusNode) Check() error {
+	if b.ConnectorId == "" {
+		return errors.New("BusNode ConnectorId undefined")
+	}
+	return nil
 }
