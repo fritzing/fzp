472f8dc5ee7178afbc273f31c3a85bbe61958cbb
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2016-06-18 08:23:19 +0200

    create extension IsValid func (cleanup old code)

--- src/go/ext/ext.go
+++ src/go/ext/ext.go
@@ -6,39 +6,34 @@ import (
 )
 
 const (
-	// ExtFzp store the .fzp extension ast constant variable
+	// ExtFzp store the .fzp extension as constant variable
 	ExtFzp = ".fzp"
-	// ExtJSON store the .fzp extension ast constant variable
+	// ExtJSON store the .json extension as constant variable
 	ExtJSON = ".json"
-	// ExtYAML store the .fzp extension ast constant variable
+	// ExtYAML store the .yml extension as constant variable
 	ExtYAML = ".yml"
 )
 
-func FilenameEqualToExt(filename, ext string) bool {
-	if filepath.Ext(filename) == ext {
-		return true
+var errorUseLowercase = errors.New("Not common file extension! Please use lowercase for the extension")
+
+// IsValid checks if a filename extension is valid. if not, return a description about the issue
+func IsValid(filename string) (bool, error) {
+	fileExt := filepath.Ext(filename)
+	switch fileExt {
+
+	case ExtFzp, ExtJSON, ExtYAML:
+		return true, nil
+
+	case ".FZP":
+		return false, errorUseLowercase
+	case ".JSON":
+		return false, errorUseLowercase
+	case ".YML", ".YAML", ".yaml":
+		return false, errorUseLowercase
+	case ".XML", ".xml":
+		return false, errors.New("Not common file extension! Use .fzp instead of " + fileExt)
+
+	default:
+		return false, errors.New("Unknown file extension! (" + fileExt + ")")
 	}
-	return false
-}
-
-// HasExtFzp returns true if the filename has a .fzp suffix
-func HasExtFzp(filename string) bool {
-	return FilenameEqualToExt(filename, ExtFzp)
-}
-
-// HasExtJSON returns true if the filename has a .json suffix
-func HasExtJSON(filename string) bool {
-	return FilenameEqualToExt(filename, ExtJSON)
-}
-
-// HasExtYAML returns true if the filename has a .yml suffix
-func HasExtYAML(filename string) bool {
-	return FilenameEqualToExt(filename, ExtYAML)
-}
-
-func IsFormatSupported(filename string) error {
-	if !HasExtFzp(filename) && !HasExtJSON(filename) && !HasExtYAML(filename) {
-		return errors.New("format not supported")
-	}
-	return nil
 }
