5af90f31ceb90c6db99a9c2310daf950c3c62adc
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2016-06-29 08:56:09 +0200

    tags check if tag already exist at the array

--- src/go/tags.go
+++ src/go/tags.go
@@ -1,9 +1,51 @@
 package fzp
 
+import (
+	"errors"
+	// "fmt"
+	"sort"
+)
+
 type Tags []string
 
 func NewTags() *Tags {
 	t := Tags{}
 	t = make([]string, 0)
 	return &t
+}
+
+func (t *Tags) Total() int {
+	return len(*t)
+}
+
+func (t *Tags) Exist(tag string) int {
+	count := 0
+	for _, v := range *t {
+		if v == tag {
+			count++
+		}
+	}
+	return count
+}
+
+func (t *Tags) Add(tag string) error {
+	if t.Exist(tag) != 0 {
+		return errors.New("tag name '" + tag + "' already exist")
+	}
+	*t = append(*t, tag)
+	return nil
+}
+
+func (t *Tags) Check() error {
+	for _, v := range *t {
+		total := sort.SearchStrings(*t, v)
+		// fmt.Println("total:", total)
+		if total != 0 {
+			// fmt.Println("DUPLICATE", v)
+
+			// TODO: collect all errors and return after for loop was finished
+			return errors.New("duplicated tag: " + v)
+		}
+	}
+	return nil
 }
--- src/go/tags_test.go
+++ src/go/tags_test.go
@@ -1,1 +1,34 @@
 package fzp
+
+import (
+	"testing"
+)
+
+func Test_Tags(t *testing.T) {
+	tags := NewTags()
+	if tags.Total() != 0 {
+		t.Error("Tags Total not equal")
+	}
+
+	tags.Add("helllo")
+	tags.Add("world")
+	if tags.Total() != 2 {
+		t.Error("Tags Total not equal")
+	}
+
+	err := tags.Add("world")
+	if err == nil {
+		t.Error("Tags Add 'world' missing error (world already exist)")
+	}
+}
+
+func Test_Tags_Check(t *testing.T) {
+	tags := Tags{}
+	tags = []string{"hello", "world", "foo", "hello"}
+	err := tags.Check()
+	// t.Log(err)
+	if err == nil {
+		t.Error("missing error (hello duplicated)", err)
+	}
+
+}
